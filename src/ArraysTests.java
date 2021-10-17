328b68a5929c1cb9acdb60cd44dc65dc0b9761dd
katiaGurtman <katia.gurtman@gmail.com>
2021-10-17 18:39:17 +0300

    clears

--- src/ArraysTests.java
+++ src/ArraysTests.java
@@ -8,90 +8,107 @@ class ArraysTests {
 
 	@Test
 	void simpleArrayTests() {
-		int ar[] = new int[11];
-		ar[10] = 2;
-		assertEquals(2, ar[10]);
+		int ar[] = new int[10];
+		ar[9] = 2;
+		assertEquals(2, ar[9]);
+		int ar1[] = ar;
+		ar1[9] = 20;
+		assertEquals(20, ar1[9]);
+		assertEquals(20, ar[9]);
+		
 	}
 	@Test
-	void swapTests() {
+	void swapTests () {
 		int ar[] = {-10, 20, 12, 40};
 		assertEquals(40, ar[3]);
 		assertEquals(12, ar[2]);
-		// swap
+		//swap
 		int tmp = ar[3];
 		ar[3] = ar[2];
 		ar[2] = tmp;
 		assertEquals(12, ar[3]);
 		assertEquals(40, ar[2]);
 	}
-	
+	@Test
+	void twoDimensional1() {
+		int ar[][] = {{1, 2, 3}, {10, 20 , 30}, {100, 200, 300}};
+		assertEquals(20, ar[1][1]);
+	}
+	@Test
+	void twoDimensional2() {
+		int ar1[] = {10, -3, 8};
+		int ar2[] = {-7, 11};
+		int ar3[] = {1, 2, 3 ,4};
+		int ar[][] = {ar1, ar2, ar3};
+		ar[1][1] = 10;
+		assertEquals(10, ar2[1]);
+	}
 	@Test
 	void systemArrayCopy1() {
 		int ar[] = new int[5];
 		int ar1[] = {1, 2, 3};
 		int ar2[] = {4, 5};
 		int expected[] = {1, 2, 3, 4, 5};
-		// TODO
 		System.arraycopy(ar1, 0, ar, 0, ar1.length);
 		System.arraycopy(ar2, 0, ar, 3, ar2.length);
 		assertArrayEquals(expected, ar);
+		
 	}
-	
 	@Test
 	void systemArrayShift() {
 		int ar[] = {1, 2, 3, 4, 5};
-		System.arraycopy(ar, 1, ar, 0, ar.length - 1);
+		System.arraycopy(ar, 1, ar, 0, ar.length - 1); //{2, 3, 4, 5, 5};
 		ar[ar.length - 1] = 0;
 		int expected[] = {2, 3, 4, 5, 0};
 		assertArrayEquals(expected, ar);
 	}
-	
 	@Test
 	void systemArrayDelete() {
-		int ar[] = {1, 2, 3, 4, 5};
+		int ar[]= {1, 2, 3, 4, 5};
 		int actual[] = new int[4];
 		int expected[] = {1, 2, 4, 5};
-		System.arraycopy(expected, 0, actual, 0, 2);
-		System.arraycopy(expected, 2, actual, 2, 2);
+		int indDelete = 2;
+		System.arraycopy(ar, 0, actual, 0, indDelete);
+		System.arraycopy(ar, 3, actual, indDelete, actual.length - indDelete);
 		assertArrayEquals(expected, actual);
 	}
-	
 	@Test
 	void systemArrayInsert() {
-		int ar[] = {1, 2, 3, 4, 5};
+		int ar[]= {1, 2, 3, 4, 5};
 		int actual[] = new int[6];
-		int expected[] = {1, 2, 3, -10, 4, 5};
-		System.arraycopy(expected, 0, actual, 0, 3);
-		System.arraycopy(expected, 4, actual, 4, 2);
+		int expected[] = {1, 2, 3,-10, 4, 5};
+		int indInsert = 3;
+		System.arraycopy(ar, 0, actual, 0, indInsert);
+		System.arraycopy(ar, indInsert, actual, indInsert + 1, ar.length - indInsert);
 		actual[3] = -10;
 		assertArrayEquals(expected, actual);
 	}
-	
 	@Test
 	void arraysCopyOf() {
-		int expected[] = {1, 2, 3, 4, 5};
-		int actual[] = Arrays.copyOf(expected, expected.length);
-		assertArrayEquals(expected, actual);
-	}
+		int ar[] = {1, 2, 3, 4, 5};
+		int expectedFull[] =  {1, 2, 3, 4, 5};
+		int expectedLess[] = {1, 2, 3};
+		int expectedGreater[] =  {1, 2, 3, 4, 5, 0, 0};
+		assertArrayEquals(expectedFull, Arrays.copyOf(ar, ar.length));
+		assertArrayEquals(expectedLess, Arrays.copyOf(ar, ar.length - 2));
+		assertArrayEquals(expectedGreater, Arrays.copyOf(ar, ar.length + 2));
 	
+	}
 	@Test
 	void arraysCopyOfRange() {
 		int ar[] = {1, 2, 3, 4, 5};
-		int expected[] = {3, 4};
-		int actual[] = Arrays.copyOfRange(ar, 2, 4);
-		assertArrayEquals(expected, actual);
+		int expected[] = {2, 3, 4};
+		assertArrayEquals(expected, Arrays.copyOfRange(ar, 1, 4));
 	}
 	
+
 	@Test
 	void arraysBinarySearch() {
-		int ar[] = {1, 2, 3, 4, 5};
-		int expected = 3;
-		int actualHas = Arrays.binarySearch(ar, 4);
-		assertEquals(expected, actualHas);
-		expected = -6;
-		int actualHasnt = Arrays.binarySearch(ar, 6);
-		assertEquals(expected, actualHasnt);
-		
+		int ar[] = {10, 20, 30, 40, 50};
+		assertEquals(1, Arrays.binarySearch(ar, 20));
+		assertEquals(-1, Arrays.binarySearch(ar, 5));
+		assertEquals(-6, Arrays.binarySearch(ar, 60));
+		assertEquals(-3, Arrays.binarySearch(ar,25));
 	}
 
 }
